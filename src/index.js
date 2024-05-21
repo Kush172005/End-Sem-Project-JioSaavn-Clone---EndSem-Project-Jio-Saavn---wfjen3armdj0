7c1d6a3de8cdd7d716afae7b1efa06261323ac84
Kush Agarwal <agrawalkush783@gmail.com>
2024-05-22 03:05:35 +0530

    Completed task

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './css/main.css';
+import ReactDOM from 'react-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+    <ToastContainer 
+     position="bottom-right"
+     autoClose={2000}
+     newestOnTop={false}
+     closeOnClick
+     rtl={false}
+     pauseOnHover
+     />
+  </React.StrictMode>,
 );
